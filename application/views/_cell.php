279f148c5a609d3d7364e537d988491bd8d10aeb
FriendlyNPC <Devanyim@gmail.com>
2015-09-18 20:36:36 -0700

    finished basic03

---
+++ application/views/_cell.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of _cell
+ *
+ * @author Devan
+ */
+?>
+<a href="./data/{filename}" data-lightbox="gallery" 
+        data-title="{title} ... posted {uploaded} by {uploader}, {category}" >
+    <img src="./data/thumb/{filename}"/>
+</a>
